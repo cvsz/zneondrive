f28ed41773f31940ad9dd951aad7e0b84c94b65d
cvsz <seaza@msn.com>
2026-09-12 23:01:21 +0700

    build: add Unreal HTTP and JSON dependencies

--- game/Source/NeonDrive/NeonDrive.Build.cs
+++ game/Source/NeonDrive/NeonDrive.Build.cs
@@ -11,7 +11,10 @@ public class NeonDrive : ModuleRules
             "Core",
             "CoreUObject",
             "Engine",
-            "InputCore"
+            "InputCore",
+            "HTTP",
+            "Json",
+            "JsonUtilities"
         });
     }
 }
