ce336b25e768799c5180deca7c408979d57be961
cvsz <seaza@msn.com>
2026-09-12 21:34:07 +0700

    feat: add replicated vehicle prototype pawn

---
+++ game/Source/NeonDrive/NDVehiclePawn.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "NDVehiclePawn.generated.h"
+
+class UBoxComponent;
+class UCameraComponent;
+class USpringArmComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class NEONDRIVE_API ANDVehiclePawn : public APawn
+{
+    GENERATED_BODY()
+
+public:
+    ANDVehiclePawn();
+    virtual void Tick(float DeltaSeconds) override;
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+protected:
+    UPROPERTY(VisibleAnywhere, Category = "Vehicle")
+    TObjectPtr<UBoxComponent> Collision;
+
+    UPROPERTY(VisibleAnywhere, Category = "Vehicle")
+    TObjectPtr<UStaticMeshComponent> Body;
+
+    UPROPERTY(VisibleAnywhere, Category = "Camera")
+    TObjectPtr<USpringArmComponent> SpringArm;
+
+    UPROPERTY(VisibleAnywhere, Category = "Camera")
+    TObjectPtr<UCameraComponent> Camera;
+
+    UPROPERTY(Replicated)
+    float AuthoritativeThrottle = 0.0f;
+
+    UPROPERTY(Replicated)
+    float AuthoritativeSteering = 0.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Prototype Driving")
+    float MaxSpeedCmPerSecond = 1400.0f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Prototype Driving")
+    float TurnRateDegreesPerSecond = 75.0f;
+
+    void InputThrottle(float Value);
+    void InputSteering(float Value);
+
+    UFUNCTION(Server, Reliable)
+    void ServerSetDrivingInput(float Throttle, float Steering);
+};
