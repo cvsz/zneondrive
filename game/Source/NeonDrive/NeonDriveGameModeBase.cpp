89493da0a6c1cdd4211e9401480b073ba5122451
cvsz <seaza@msn.com>
2026-09-13 14:55:55 +0700

    feat(ue): record server tick and session telemetry

--- game/Source/NeonDrive/NeonDriveGameModeBase.cpp
+++ game/Source/NeonDrive/NeonDriveGameModeBase.cpp
@@ -1,9 +1,34 @@
 #include "NeonDriveGameModeBase.h"
 #include "NDPlayerController.h"
+#include "NDServerTelemetry.h"
 #include "NDVehiclePawn.h"
 
 ANeonDriveGameModeBase::ANeonDriveGameModeBase()
 {
+    PrimaryActorTick.bCanEverTick = true;
     DefaultPawnClass = ANDVehiclePawn::StaticClass();
     PlayerControllerClass = ANDPlayerController::StaticClass();
+}
+
+void ANeonDriveGameModeBase::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    FNDServerTelemetry::RecordServerTick(DeltaSeconds);
+    if (const UWorld* World = GetWorld())
+    {
+        FNDServerTelemetry::MaybeLog(World->GetTimeSeconds());
+    }
+}
+
+void ANeonDriveGameModeBase::PostLogin(APlayerController* NewPlayer)
+{
+    Super::PostLogin(NewPlayer);
+    FNDServerTelemetry::RecordPlayerJoin();
+}
+
+void ANeonDriveGameModeBase::Logout(AController* Exiting)
+{
+    FNDServerTelemetry::RecordPlayerLeave();
+    Super::Logout(Exiting);
 }
