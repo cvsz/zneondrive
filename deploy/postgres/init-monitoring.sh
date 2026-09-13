1412704c598f509b51860040c387e367aa9d393f
cvsz <seaza@msn.com>
2026-09-14 06:50:11 +0700

    feat(observability): add least-privilege postgres monitor init

---
+++ deploy/postgres/init-monitoring.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+
+: "${POSTGRES_EXPORTER_PASSWORD:?POSTGRES_EXPORTER_PASSWORD must be set}"
+
+psql -v ON_ERROR_STOP=1 \
+  --username "$POSTGRES_USER" \
+  --dbname "$POSTGRES_DB" \
+  --set=monitor_password="$POSTGRES_EXPORTER_PASSWORD" <<'SQL'
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'zneondrive_monitor') THEN
+    CREATE ROLE zneondrive_monitor LOGIN;
+  END IF;
+END
+$$;
+ALTER ROLE zneondrive_monitor NOSUPERUSER NOCREATEDB NOCREATEROLE NOREPLICATION NOBYPASSRLS;
+ALTER ROLE zneondrive_monitor PASSWORD :'monitor_password';
+GRANT pg_monitor TO zneondrive_monitor;
+SQL
