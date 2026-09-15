5b4628184a28b1dcaac4e7c3b520da946941d52c
cvsz <seaza@msn.com>
2026-09-16 04:14:47 +0700

    feat(game): add data-driven 3D character definition

---
+++ game/Source/NeonDrive/NDCharacterDefinition.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "NDCharacterDefinition.generated.h"
+
+class UAnimInstance;
+class USkeletalMesh;
+
+UCLASS(BlueprintType)
+class NEONDRIVE_API UNDCharacterDefinition : public UPrimaryDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Identity")
+    FName CharacterId;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Identity")
+    FText DisplayName;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Identity")
+    FName FactionId;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Identity")
+    FName ArchetypeId;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Visual")
+    TSoftObjectPtr<USkeletalMesh> BodyMesh;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
+    TSoftClassPtr<UAnimInstance> AnimationClass;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
+    FName LocomotionProfileId = TEXT("default");
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Visual", meta = (ClampMin = "0.5", ClampMax = "2.0"))
+    float CharacterScale = 1.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Visual")
+    bool bUseHighFidelityFacialRig = false;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Network")
+    bool bReplicateCosmeticState = true;
+
+    virtual FPrimaryAssetId GetPrimaryAssetId() const override;
+};
