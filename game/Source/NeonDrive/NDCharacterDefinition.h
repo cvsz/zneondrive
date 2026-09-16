ba6efcef741d49ed4b17a2c3ccdf25bf327fa693
cvsz <seaza@msn.com>
2026-09-16 09:53:21 +0700

    feat: expand character presentation runtime contract

--- game/Source/NeonDrive/NDCharacterDefinition.h
+++ game/Source/NeonDrive/NDCharacterDefinition.h
@@ -7,6 +7,15 @@
 class UAnimInstance;
 class USkeletalMesh;
 
+UENUM(BlueprintType)
+enum class ENDCharacterPresentationTier : uint8
+{
+    Crowd,
+    NPC,
+    Player,
+    Hero
+};
+
 UCLASS(BlueprintType)
 class NEONDRIVE_API UNDCharacterDefinition : public UPrimaryDataAsset
 {
@@ -34,14 +43,35 @@ public:
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
     FName LocomotionProfileId = TEXT("default");
 
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation")
+    FName MotionMatchingProfileId = TEXT("humanoid_default");
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Rig")
+    FName SkeletonProfileId = TEXT("neondrive_humanoid_v1");
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Facial")
+    FName FacialProfileId = TEXT("none");
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Visual")
+    ENDCharacterPresentationTier PresentationTier = ENDCharacterPresentationTier::NPC;
+
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Visual", meta = (ClampMin = "0.5", ClampMax = "2.0"))
     float CharacterScale = 1.0f;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Visual")
     bool bUseHighFidelityFacialRig = false;
 
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Visual")
+    bool bUseGroom = false;
+
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Network")
     bool bReplicateCosmeticState = true;
+
+    UFUNCTION(BlueprintPure, Category = "Character")
+    bool HasValidIdentity() const;
+
+    UFUNCTION(BlueprintPure, Category = "Character")
+    bool UsesHighFidelityPresentation() const;
 
     virtual FPrimaryAssetId GetPrimaryAssetId() const override;
 };
