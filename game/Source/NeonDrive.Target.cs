185e8b34f719057e15b12a540a35d9f53c7bba3e
cvsz <seaza@msn.com>
2026-09-18 02:48:30 +0000

    chore: update project configuration and web assets

--- game/Source/NeonDrive.Target.cs
+++ game/Source/NeonDrive.Target.cs
@@ -6,7 +6,7 @@ public class NeonDriveTarget : TargetRules
     public NeonDriveTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("NeonDrive");
     }
--- game/Source/NeonDriveEditor.Target.cs
+++ game/Source/NeonDriveEditor.Target.cs
@@ -6,7 +6,7 @@ public class NeonDriveEditorTarget : TargetRules
     public NeonDriveEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("NeonDrive");
     }
